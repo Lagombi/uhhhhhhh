b07df0050c656ab6fe97c0d1cd7d59619aaa5e36
Chase Goehring <cgoehring@smu.edu>
2018-08-16 14:56:06 -0500

    Add files via upload
    oops i fixed the database

--- leads.sql
+++ leads.sql
@@ -1,2 +1,2 @@
 CREATE TABLE leads
-(name varchar(255), address varchar(255), email varchar(255), phone varchar(255), type ENUM("Stone Supplier", "Hotel", "Fabricator", "Existing Client", "Designer"), noSent int, lastSent DATE, hasResponded boolean, PRIMARY KEY (name));
+(name varchar(255), address varchar(255), email varchar(255), phoneNumber varchar(255), type ENUM("Stone Supplier", "Hotel", "Fabricator", "Existing Client", "Designer"), noSent int, lastSent DATE, hasResponded boolean, PRIMARY KEY (name));
